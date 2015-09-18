2a8e23b493c6e033ed177270b83cba6ab916de1e
chaoys <623776401@qq.com>
2015-09-18 12:25:22 +0800

    support subdir; uri decode; utf8 msg len; code clean;

--- ncget.sh
+++ ncget.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# silly HTTP/GET file server powered by netcat
+# silly HTTP/GET file server
 # Date 2015年 09月 05日 星期六 18:54:23 CST
 # Author 晁永生
 
@@ -8,118 +8,113 @@
 # $ mkfifo /tmp/f
 # $ cat /tmp/f | hfs.sh <path to html root> | netcat -l 8080 >/tmp/f
 
-code=200
+dbg()
+{
+	echo "DEBUG: $*" >/dev/stderr
+}
 
 response()
 {
+	code=$1
+
+	[ $code -eq 200 ] && { printf "HTTP/1.1 200 OK\r\n"; return; }
+
 	case $code in
-	200)
-	printf "HTTP/1.1 200 OK\r\n"
-	;;
 	400)
 	printf "HTTP/1.1 400 Bad Request\r\n"
-	msg="<html><body><p>400</p></body></html>"
-	printf "Content-Type: text/html\r\n"
-	printf "Content-Length: %d\r\n" ${#msg}
-	printf "\r\n%s" "$msg"
 	;;
 	403)
 	printf "HTTP/1.1 403 Forbidden\r\n"
-	msg="<html><body><p>403</p></body></html>"
-	printf "Content-Type: text/html\r\n"
-	printf "Content-Length: %d\r\n" ${#msg}
-	printf "\r\n%s" "$msg"
 	;;
 	404)
 	printf "HTTP/1.1 404 Not Found\r\n"
-	msg="<html><body><p>404</p></body></html>"
-	printf "Content-Type: text/html\r\n"
-	printf "Content-Length: %d\r\n" ${#msg}
-	printf "\r\n%s" "$msg"
 	;;
 	405)
 	printf "HTTP/1.1 405 Method Not Allowed\r\n"
-	msg="<html><body><p>405</p></body></html>"
-	printf "Content-Type: text/html\r\n"
-	printf "Content-Length: %d\r\n" ${#msg}
-	printf "\r\n%s" "$msg"
 	;;
 	505)
 	printf "HTTP/1.1 505 HTTP Version Not Supported\r\n"
-	msg="<html><body><p>505</p></body></html>"
-	printf "Content-Type: text/html\r\n"
-	printf "Content-Length: %d\r\n" ${#msg}
-	printf "\r\n%s" "$msg"
 	;;
 	*)
 	;;
 	esac
+
+	msg="<html><body><p>${code}</p></body></html>"
+	printf "Content-Type: text/html\r\n"
+	printf "Content-Length: %d\r\n" ${#msg}
+	printf "\r\n%s" "$msg"
 }
 
 list()
 {
-	response
-	msg="<html><body><ul>"
-	for f in *; do
-		msg=${msg}"<li><a href=\"$f\">$f</a></li>"
-	done
-	msg=${msg}"</body></html>"
+	p="${1%/}"
+	pushd $p >/dev/null
 
+	msg="<html><body><ul>"
+	for f in *;do
+		msg=${msg}"<li><a href=\"${p}/$f\">$f</a></li>"
+	done
+	msg=${msg}"</ul></body></html>"
+
+	totlen=`expr length "${msg}"`
+
+	response 200
 	printf "Content-Type: text/html; charset=utf-8\r\n"
-	printf "Content-Length: %d\r\n" $((${#msg}))
+	printf "Content-Length: %d\r\n" $totlen
 	printf "\r\n"
 	printf "%s" "${msg}"
+
+	popd >/dev/null
 }
 
 sendfile()
 {
 	f=$1
-	totlen=`ls -l $f | awk '{print $5}'`
+	totlen=`ls -l "$f" | awk '{print $5}'`
 
-	response
+	response 200
 	printf "Content-Type: application/octet-stream\r\n"
 	printf "Content-Length: %d\r\n" ${totlen}
 	printf "\r\n"
-	dd if=$f bs=4096 count=$((totlen/4096)) 2>/dev/null
-	dd if=$f skip=$((totlen/4096)) bs=4096 count=1 2>/dev/null
+	dd if="$f" bs=4096 count=$((totlen/4096)) 2>/dev/null
+	dd if="$f" skip=$((totlen/4096)) bs=4096 count=1 2>/dev/null
 }
 
-parse_http()
+http()
 {
-#filter request field
+	#filter request field
 	idx=`expr index $1 ":"`
 	[ $idx -eq 0 ] || return
 	
-#blank line, '\r'
+	#blank line, '\r'
 	[ ${#1} -eq 1 ] && return
 
-	[ $# -eq 3 ] || { code=400; response; return; }
-	[ "$1" == "GET" ] || { code=405; response; return; }
+	[ $# -eq 3 ] || { response 400; return; }
+	[ "$1" == "GET" ] || { response 405; return; }
 	v=${3:0:8}
-	[ "$v" == "HTTP/1.0" ] || [ "$v" == "HTTP/1.1" ] || [ "$v" == "HTTP/2.0" ] || { code=505; response; return; }
+	[ "$v" == "HTTP/1.0" ] || [ "$v" == "HTTP/1.1" ] || [ "$v" == "HTTP/2.0" ] || { response 505; return; }
 
 	uri=$2
-	code=200
 	uri=${uri#'/'}
-	if [ "$uri" == "" ]; then
-		list
-		return
-	fi
-	idx=`expr index $uri "/"`
-	[ $idx -eq 0 ] || { code=403; response; return; }
-	[ -e $uri ] || { code=404; response; return; }
 
-	sendfile $uri
+	uri=$(perl -e "use URI::Escape;print uri_unescape(\"$uri\")")
+
+	[ "$uri" == "" ] && { list "./"; return; }
+	[ -e "$uri" ] || { response 404; return; }
+	[ -f "$uri" ] && { sendfile "$uri"; return; }
+	[ -d "$uri" ] && { list "$uri"; return; }
+	response 400
 }
 
 # entry
 
 [ $# -eq 1 ] || return
-html_root=$1
-[ -e ${html_root} ] || return
-cd ${html_root}
+rootdir=$1
+[ -e ${rootdir} ] || return
+cd ${rootdir}
 
 while read line; do
-	parse_http $line
+	dbg $line
+	http $line
 done
 
